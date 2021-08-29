dda145d397af4ec8d44ad2e2d150dd9a49eda3d4
Wo1fz <nmy_97@hotmail.com>
2021-08-29 23:38:36 +0800

    implement update post function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,6 @@
 import PostMessage from '../models/PostMessage.js'
+import mongoose from 'mongoose'
+import express from 'express'
 
 export const getPosts = async (req, res) => {
   try {
@@ -28,4 +30,22 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params
+  const post = req.body
+
+  if (!mongoose.isValidObjectId(_id))
+    return res.status(404).send('No post with that id')
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  )
+
+  res.json(updatedPost)
 }
